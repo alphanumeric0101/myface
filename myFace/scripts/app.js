7504faad114c7d30c37545b22536ea7873c4ea4e
Enzo Matrix <alphanumeric@openmailbox.org>
2015-12-28 16:18:53 +0000

    mostly functional login

--- myFace/scripts/app.js
+++ myFace/scripts/app.js
@@ -1,22 +1,37 @@
 Parse.initialize("smv6lK9yWTaYyLWoONESdxxHU1hGTOGGc09TB9TG", "NIYUafzMVQYSSuyiFwRTxtdWQ3mQb4c8X59CNpf7");
+var History = ReactRouter.History;
 
 const app = React.createClass({
+  mixins: [ParseReact.Mixin],
   getInitialState: function() {
     return {
-      currentUser: Parse.User.current()
+      error: null,
+      signup: false
     };
   },
+  observe: function() {
+    return {
+      user: ParseReact.currentUser
+      //posts: ParseReact.currentUser.posts
+    };
+  },
+  // getUserContent: function(user) {
+  //   // get all the user data we need and save it as a prop we can pass on to Posts
+  //   return "content";
+  // },
+
   render: function() {
-    if (this.state.currentUser) {
-      return <div><Posts userId="11"/></div>;
+    if (this.data.user) {
+      return <div><Posts userData={'somedata'} user={this.data.user}/></div>;
     }
     else {
-    return <div><h1>Welcome to PostBox!</h1> {this.props.children}</div>;
+      return <div><h1>Welcome to PostBox!</h1><Login/></div>;
     }
   }
 });
 
-const Welcome = React.createClass({
+const Login = React.createClass({
+  mixins: [History],
   getInitialState: function() {
     return {
       email: '',
@@ -39,23 +54,13 @@ const Welcome = React.createClass({
   /*LOGIN FUNCTION*/
 
   logIn: function() {
-    console.log('calling logIn');
-    Parse.User.logIn(this.state.email, this.state.password, {
-      success: function(user) {
-        // Do stuff after successful login. Redirect to Home page...
-        alert('welcome back!');
-        window.location = "?#account";
-      },
-      error: function(user, error) {
+    Parse.User.logIn(this.state.email, this.state.password).then(function(user) {
+      alert('welcome back!');
+    }, function(user, error) {
         alert('bad login, check your inputs');
-      }
-    });
+      });
   },
-  renderRegister: function() {
-    window.location = "?#signup";
-  },
-
-  /*RENDERING THE PAGE*/
+  /*RENDERING THE LOGIN PAGE*/
 
   render: function() {
     return (
@@ -101,6 +106,7 @@ const Welcome = React.createClass({
 });
 
 const Register = React.createClass({
+  mixins: [History],
   getInitialState: function() {
     return {
       username: '',
@@ -125,10 +131,10 @@ const Register = React.createClass({
       confPw: e.target.value
     });
   },
-  
+
   signUp: function(e) {
     e.preventDefault();
-    console.log('called signUp function');
+    console.log('called signUp function' + this.refs);
     var user = new Parse.User();
     user.set("username", this.state.email);
     user.set("password", this.state.password);
@@ -138,14 +144,14 @@ const Register = React.createClass({
       success: function(user) {
         // Hooray! Let them use the app now.
         alert('you have signed up succesfully');
-        window.location = "?#account";
       },
       error: function(user, error) {
         // Show the error message somewhere and let the user try again.
         alert("Error: " + error.code + " " + error.message);
       }
     });
-    
+
+    //this.history.pushState(null, '/account');
   },
   /*RENDERING THE REGISTER PAGE*/
 
@@ -194,7 +200,7 @@ const Register = React.createClass({
 });
 
 const Posts = React.createClass({
-
+  
   getInitialState: function() {
     return {
       text: "",
@@ -205,7 +211,7 @@ const Posts = React.createClass({
       body: ''
     };
   },
-  
+
   handleChange: function(event) {
     this.setState({
       text: event.target.value
@@ -214,40 +220,33 @@ const Posts = React.createClass({
   toggleFile: function(event) {
     this.setState({
       fileAdded: !this.state.fileAdded
-    }); // POSTS SECTION
+    });
   },
   togglePrivate: function(event) {
+    console.log(this.props);
     this.setState({
       isPrivate: !this.state.isPrivate
     });
   },
   logout: function() {
     Parse.User.logOut();
-  },
-  createPost: function() {
+  },                        // POSTS SECTION ///////////////////
+  createPost: function(e) {
+    e.preventDefault();
     var allPosts
     var user = Parse.User.current();
     var Post = Parse.Object.extend("Post");
     var post = new Post();
-    post.set("content", this.state.text);
+    post.set("body", this.state.text);
     post.set("user", user);
+    post.set("private", this.state.isPrivate);
     post.save(null, {
-  success: function(post) {
-    // Find all posts by the current user
-    var query = new Parse.Query(Post);
-    query.equalTo("user", user);
-    query.find({
-      success: function(usersPosts) {
-        // userPosts contains all of the posts by the current user.
-        console.log(usersPosts);
-        allPosts = usersPosts
+      success: function(post) {
+        alert(post + "was saved");
       }
     });
-  }
-});
-this.setState({userPosts:allPosts});
   },
-  
+
   render: function() {
     return (
       <div className="well clearfix">
@@ -270,7 +269,7 @@ this.setState({userPosts:allPosts});
               {this.state.fileAdded ? <span className="fa fa-plus-circle"></span> : <span className="fa fa-paperclip"></span> }
             </button>
        </form>
-       <div><Content/></div>
+       <div><Content userId={this.props.userId} data={this.props.userData}/></div>
       </div>
     );
   }
@@ -279,99 +278,38 @@ this.setState({userPosts:allPosts});
 const Content = React.createClass({
   getInitialState: function() {
     return {
-      usersPosts: ''
+      usersPosts: {},
     };
   },
-  retrievePosts: function() {
-    console.log('calling getPosts');
-    var Post = Parse.Object.extend("Post");
-var query = new Parse.Query(Post);
-var postObject = []
-query.find({
-  success: function(results) {
-    alert("Successfully retrieved " + results.length + " Posts.");
-    // Do something with the returned Parse.Object values
-    for (var i = 0; i < results.length; i++) {
-      postObject.push(results[i]);
-      var object = results[i];
-      console.log(object.id + ' - ' + object.get('content'));
-    }
-    
-  },
-  error: function(error) {
-    alert("Error: " + error.code + " " + error.message);
-  }
-});
-  this.setState({usersPosts: postObject});
-  return this.usersPosts;
-  },
   render: function() {
-    console.log(this.props.usersPosts);
-    return(
-    // <button className="btn btn-priamry" onClick={this.retrievePosts}>getPostsyo</button>
-    <div>{this.props.usersPosts}</div>
-    
+    return (
+      <div>
+      <p>These are your posts...</p>
+      <p>{this.props.userData}</p>
+      </div>
     );
   }
 });
 
 function checkLoggedIn(nextState, replaceState) {
-  var user = Parse.User.Current();
-  console.log(Parse.User.Current);
-  if (user) {
-    replaceState(null, '/account');
+  var user = Parse.User.current();
+  console.log(user);
+  if (!user) {
+    replaceState(null, '/signup');
   }
-
 }
-
-/*var Login = React.createClass({
-  render : function() {
-    return (
-    <div className="well clearfix">
-    
-    <form className="form-horizontal">
-    
-    <div className="input-group">
-      <span className="input-group-addon"><i className="fa fa-at"></i></span>
-      <input type="text" className="form-control" placeholder=" Email"></input>
-    </div>
-
-    <div className="input-group">
-      <span className="input-group-addon"><i className="fa fa-key"></i></span>
-      <input type="text" className="form-control" placeholder=" Password"></input>
-    </div>
-    
-        <br/>
-      
-        <button className="btn btn-success btn-md btn-block" id="signIn-btn">Sign In
-          <span className="fa fa-sign-in"></span>
-        </button>
-        
-        <button className="btn btn-info btn-md btn-block" id="register-btn">Register
-          <span className="fa fa-user-plus"></span>
-        </button>
-      </form>
-      </div>
-      );
-  }
-});*/
 
 let Router = ReactRouter.Router;
 let Route = ReactRouter.Route;
 let IndexRoute = ReactRouter.IndexRoute;
 let Link = ReactRouter.Link;
-let browserHistroy = ReactRouter.History;
-
 
 ReactDOM.render((
   <Router>
-    <Route path="/" component={app}>
-      <IndexRoute component={Welcome} onEnter={checkLoggedIn}/>
-      
+    <Route path="/" component={app}/>
+      <Route path="account" component={Posts}/>
+      <Route path="login" component={Login}/>
       <Route path="signup" component={Register}/>
-      <Route path="account" component={app}/>
-      
       <Route path="*" component={app}/>
-    </Route>
   </Router>
 ), document.getElementById("content"));
